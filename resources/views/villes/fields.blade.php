20dc7da2726cad2700ebaa531716888b23b8cb52
Colin <colinraminoson@gmail.com>
2024-05-12 19:33:13 +0300

    init

--- resources/views/villes/fields.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- Nom Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nom', 'Nom:') !!}
-    {!! Form::text('nom', null, ['class' => 'form-control', 'maxlength' => 255,'required' => true]) !!}
-</div>
